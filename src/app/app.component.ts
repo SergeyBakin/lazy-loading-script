9b2ed3c603082cb32115ddec74d69631611ee2cc
Бакин Сергей Сергеевич <SBakin@nvg.ru>
2018-11-07 01:17:13 +0300

    initiliazation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LazyLoadingScriptService }  from "./services/lazy-loading-script.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lazy-loading-scripts';
+  status = '';
+
+  constructor(private lazyLoadingScriptService: LazyLoadingScriptService) {}
+
+  ngOnInit() {
+      this.lazyLoadingScriptService.load('filepicker').then(data => {
+            console.log('script loaded ', data);
+        }).catch(error => console.log(error));
+  }
 }
